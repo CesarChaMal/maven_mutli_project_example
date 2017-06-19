f9365ff64aa0045588b61f1b43179f4b8153cbf4
Thomas Nield <thomasnield@live.com>
2017-06-19 10:27:48 -0500

    initial commit

---
+++ app1/src/main/java/Launcher1.java
@@ -0,0 +1,5 @@
+public class Launcher1 {
+	public static void test() {
+		System.out.println("Hello from app1");
+	}
+}
---
+++ app2/src/main/java/Launcher2.java
@@ -0,0 +1,5 @@
+public class Launcher2 {
+	public static void main(String[] args) { 
+		Launcher1.test();
+	}
+}
